f9b80c04aba3abecd33503bfbfda0c86ba7bfc90
Rodrigues dos Anjos <paas@gft.com>
2017-05-22 08:49:33 -0300

    Primeiro commit

--- teste/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('mainModule.config', [])
-
-.config(function(angularjsIssueProvider){
- angularjsIssueProvider.setConnection('https://api.github.com/repos/angular/angular.js/issues');
-})
--- teste/main.js
+++
@@ -1,7 +0,0 @@
-angular.module('mainModule', [
-    'ngResource', 
-    'mainModule.controller', 
-    'mainModule.provider', 
-    'mainModule.config'
-    ])
-
